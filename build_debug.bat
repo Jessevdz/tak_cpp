5d38ef2ed67c4c5d9166b9328a16e433e6a7bcb6
jesse <jesse_vdz@hotmail.com>
2023-02-28 09:58:25 +0100

    Fix makefile

--- build_debug.bat
+++ build_debug.bat
@@ -1,5 +1,5 @@
 rm -r build
 mkdir build
 cd build
-cmake -DCMAKE_PREFIX_PATH=C:\Users\Jesse\Projects\tak_cpp\libtorch_debug ..
+cmake -DCMAKE_PREFIX_PATH=C:\Users\Jesse\Projects\tak_cpp\lib\libtorch_debug ..
 cmake --build . --config Debug
